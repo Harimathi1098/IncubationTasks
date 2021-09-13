6fefde30390db06a681e17024f9df1a6a3b78259
Harimathi1098 <90602219+Harimathi1098@users.noreply.github.com>
2021-09-13 15:14:25 +0530

    Update AscendingOrderSorting.java

--- AscendingOrderSorting.java
+++ AscendingOrderSorting.java
@@ -3,27 +3,49 @@ package com.incubtasks;
 import java.util.Scanner;
 
 public class AscendingOrderSorting {
-	public static int[] swap(int[] integerArray,int i,int j)
+	public static void merge(int[] integerArray,int low,int middle,int high)
 	
 	{
-		int temporaryVariable=integerArray[i];
-		integerArray[i]=integerArray[j];
-		integerArray[j]=temporaryVariable;
-		return integerArray;
-	}
-	public static int[] sortInAscendingOrder(int[] integerArray)
-	{
-		for(int i=0;i<integerArray.length;i++)
+		int i=low;
+		int j=middle+1;
+		int k=low;
+		int resultArray[]=new int[high+1];
+		while(i<=middle&&j<=high)
 		{
-			for(int j=0;j<integerArray.length-1;j++)
+			if(integerArray[i]<integerArray[j])
 			{
-				if(integerArray[j]>integerArray[j+1])
-				{
-					swap(integerArray,j,j+1);
-				}
+				resultArray[k++]=integerArray[i++];
+				
+			}
+			else
+			{
+				resultArray[k++]=integerArray[j++];
 			}
 		}
-		return integerArray;
+		while(i<=middle)
+		{
+			resultArray[k++]=integerArray[i++];
+		}
+		while(j<=high)
+		{
+			resultArray[k++]=integerArray[j++];
+		}
+		for(int p=low;p<=high;p++)
+		{
+			integerArray[p]=resultArray[p];
+		}
+		
+	}
+	public static void  mergeSort(int[] integerArray,int low,int high)
+	{
+		if(low<high)
+		{
+			int middle=(low+high)/2;
+			mergeSort(integerArray,low,middle);
+			mergeSort(integerArray, middle+1, high);
+			merge(integerArray,low,middle,high);
+		}
+		
 	}
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
@@ -36,11 +58,11 @@ public class AscendingOrderSorting {
 			integerArray[i]=scan.nextInt();
 		}
 		
-		int sortedArray[]=sortInAscendingOrder(integerArray);
+		mergeSort(integerArray,0,sizeOfArray-1);
 		System.out.println("---The Given Array sorted in Ascending Order:---");
 		for(int i=0;i<integerArray.length;i++)
 		{
-		System.out.print(sortedArray[i]+" ");
+		System.out.print(integerArray[i]+" ");
 		}
 
 		}
